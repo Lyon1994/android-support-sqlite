244064e241bda8725d26a533d53744fa93685c2d
Lyon_Yan <765211630@qq.com>
2015-09-15 17:16:57 +0800

    init

---
+++ gen/org/lyon_yan/android/support/sqlite/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.lyon_yan.android.support.sqlite;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
